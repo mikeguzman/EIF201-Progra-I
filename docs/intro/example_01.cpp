7e43ae715792026cc1ef4a77fb56ca8525ff009e
Maikol <mikeguzman@gmail.com>
2019-07-29 15:34:37 -0600

    First Changes

---
+++ docs/intro/example_01.cpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  example_01.cpp
+ *
+ *    Description:  My first app to print a line
+ *
+ *        Version:  1.0
+ *        Created:  29/07/2019
+ *       Compiler:  g++
+ *
+ *         Author:  Maikol Guzmán mikeguzman@gmail.com
+ *   Organization:  LolaDog Studio
+ *
+ * =====================================================================================
+ */
+#include <iostream>  // allows program to output data to the screen
+
+// function main begins program execution
+int main(int argc, const char *argv[]) {
+    std::cout << "Welcome to C++!" << std::endl;  // display message
+
+    return 0;  // indicate that program ended successfully
+}  // end function main
---
+++ docs/intro/intro.md
@@ -0,0 +1,3 @@
+# Universidad Nacional de Costa Rica | EIF201 - Programación I
+## Introduction to C++ Programming, Input/Output and Operators
+### FIRST PROGRAM IN C++: PRINTING A LINE OF TEXT
