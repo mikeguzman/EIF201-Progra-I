f37593457886ce1d3e675d9aecfa00f4ea6269f3
Maikol <mikeguzman@gmail.com>
2019-08-05 20:58:11 -0600

    Libro de Calificaciones

--- exercises/LibroCalificaciones/LibroCalificaciones.cpp
+++ exercises/LibroCalificaciones/LibroCalificaciones.cpp
@@ -1,5 +1,67 @@
-//
-// Created by Maikol Guzman  on 2019-08-05.
-//
+/*
+ * =====================================================================================
+ *
+ *       Filename:  LibroCalificaciones.cpp
+ *
+ *    Description:  Clase de Libro Calificaciones
+ *
+ *        Created:  2019-08-05
+ *
+ *         Author:  Maikol Guzman Alan mikeguzman@gmail.com
+ *   Organization:  Universidad Nacional de Costa Rica
+ *
+ * =====================================================================================
+ */
+
 
 #include "LibroCalificaciones.h"
+#include <string>
+
+LibroCalificaciones::LibroCalificaciones() {}
+
+LibroCalificaciones::LibroCalificaciones(const std::string &nombreCurso, int nota) : nombreCurso(nombreCurso),
+                                                                                     nota(nota) {}
+
+std::string LibroCalificaciones::obtenerRangoLetra() {
+    std::string resultado;
+
+    if (getNota() >= 90 ) {
+        resultado = "A";
+    } else if (getNota() >= 80 ) {
+        resultado = "B";
+    } else if (getNota() >= 70 ) {
+        resultado = "C";
+    } else if (getNota() >= 60 ) {
+        resultado = "D";
+    } else {
+        resultado = "F";
+    }
+
+    return resultado;
+}
+
+std::string LibroCalificaciones::obtenerMensaje() {
+    std::string mensaje = "";
+
+    mensaje = "El curso " + getNombreCurso() + " con la nota " + std::to_string(getNota()) + " pertenece al rango " +
+            obtenerRangoLetra();
+
+    return mensaje;
+}
+
+
+const std::string &LibroCalificaciones::getNombreCurso() const {
+    return nombreCurso;
+}
+
+void LibroCalificaciones::setNombreCurso(const std::string &nombreCurso) {
+    LibroCalificaciones::nombreCurso = nombreCurso;
+}
+
+int LibroCalificaciones::getNota() const {
+    return nota;
+}
+
+void LibroCalificaciones::setNota(int nota) {
+    LibroCalificaciones::nota = nota;
+}
--- exercises/LibroCalificaciones/LibroCalificaciones.h
+++ exercises/LibroCalificaciones/LibroCalificaciones.h
@@ -1,12 +1,42 @@
-//
-// Created by Maikol Guzman  on 2019-08-05.
-//
+/*
+ * =====================================================================================
+ *
+ *       Filename:  LibroCalificaciones.h
+ *
+ *    Description:  Encabezado de la clase Libro Calificaciones
+ *
+ *        Created:  2019-08-05
+ *
+ *         Author:  Maikol Guzman Alan mikeguzman@gmail.com
+ *   Organization:  Universidad Nacional de Costa Rica
+ *
+ * =====================================================================================
+ */
+
 
 #ifndef LIBROCALIFICACIONES_LIBROCALIFICACIONES_H
 #define LIBROCALIFICACIONES_LIBROCALIFICACIONES_H
 
 
+#include <string>
+
 class LibroCalificaciones {
+    std::string nombreCurso;
+    int nota;
+    std::string obtenerRangoLetra();
+
+public:
+    LibroCalificaciones();
+    LibroCalificaciones(const std::string &nombreCurso, int nota);
+
+    // Métodos
+    std::string obtenerMensaje();
+
+    // Get / Set
+    const std::string &getNombreCurso() const;
+    void setNombreCurso(const std::string &nombreCurso);
+    int getNota() const;
+    void setNota(int nota);
 
 };
 
